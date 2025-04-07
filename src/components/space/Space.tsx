bd4bacd5fc047705e6d8fdfb4b39ea0f8e120205
DaeWon9 <qkreodnjs97@naver.com>
2025-04-08 04:36:40 +0900

    fix: Space component margin

--- src/components/space/Space.tsx
+++ src/components/space/Space.tsx
@@ -1,11 +1,12 @@
 import ReactImitation from "daewon-react-imitation";
+import "./space.css";
 
 interface SpaceProps {
     size: number;
 }
 
-const Space = (size: SpaceProps) => {
-    return <div style={{ margin: `${size}px 0` }} />;
+const Space = ({ size }: SpaceProps) => {
+    return <div className={`space-${size}`} />;
 };
 
 export default Space;
